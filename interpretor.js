71b02a02ce0895471364600a3328356b0226551f
Patricia Moraru <patriciamoraruv@gmail.com>
2025-04-11 20:34:18 +0300

    Edit file for importing chemistryData.js

--- interpretor.js
+++ interpretor.js
@@ -1,3 +1,15 @@
+import { 
+  periodicTable, 
+  hydrocarbonWeights, 
+  oxidizingAgents, 
+  reducingAgents, 
+  validCombustionFuels,
+  acids,
+  bases,
+  physicalConstants
+} from './chemistryData.js';
+
+
 export default class Interpretor {
   constructor(parseTree, outputCallback) {
     this.parseTree = parseTree;
@@ -180,6 +192,10 @@ export default class Interpretor {
         return this.getVolume(args[0], args[1]);
       case "getV":
         return this.getGasVolume(args[0], args[1], args[2]);
+      case "isAcid":
+        return this.isAcid(args[0]);
+      case "isBase":
+        return this.isBase(args[0]);
       default:
         throw new Error(`Unknown function: ${node.value}`);
     }
@@ -236,176 +252,12 @@ export default class Interpretor {
 
   getMolecularWeight(formula) {
     // First, try a lookup for common hydrocarbons.
-    const hydrocarbonWeights = {
-      CH4: 16.04, // Methane
-      C2H6: 30.07, // Ethane
-      C3H8: 44.1, // Propane
-      C4H10: 58.12, // Butane
-      C5H12: 72.15, // Pentane
-      C6H14: 86.18, // Hexane
-      C7H16: 100.21, // Heptane
-      C8H18: 114.22, // Octane
-      C9H20: 128.25, // Nonane
-      C10H22: 142.29, // Decane
-
-      C2H2: 26.04, // Acetylene (Ethyne)
-      C2H4: 28.05, // Ethylene (Ethene)
-      C3H6: 42.08, // Propylene (Propene)
-      C4H8: 56.11, // Butylene (Butene)
-      C5H10: 70.13, // Pentene
-      C6H12: 84.16, // Hexene
-      C7H14: 98.19, // Heptene
-      C8H16: 112.21, // Octene
-      C9H18: 126.24, // Nonene
-      C10H20: 140.27, // Decene
-
-      C6H6: 78.11, // Benzene
-      C7H8: 92.14, // Toluene
-      C8H10: 106.17, // Xylene
-      C9H12: 120.19, // Trimethylbenzene
-      C10H14: 134.22, // Tetramethylbenzene
-
-      C6H10: 82.15, // Cyclohexene
-      C6H12: 84.16, // Cyclohexane
-      C7H14: 98.19, // Cycloheptane
-      C8H16: 112.21, // Cyclooctane
-
-      C10H8: 128.17, // Naphthalene
-      C14H10: 178.23, // Anthracene
-      C14H10: 178.23, // Phenanthrene
-      C16H10: 202.26, // Pyrene
-      C18H12: 228.29, // Chrysene
-
-      C20H12: 252.31, // Benzo[a]pyrene
-      C24H12: 300.36, // Coronene
-    };
 
     if (hydrocarbonWeights[formula]) {
       return hydrocarbonWeights[formula];
     }
 
     // Otherwise, compute weight using periodic table values.
-    const periodicTable = {
-      H: 1.008,
-      He: 4.0026,
-      Li: 6.94,
-      Be: 9.0122,
-      B: 10.81,
-      C: 12.01,
-      N: 14.01,
-      O: 16.0,
-      F: 18.998,
-      Ne: 20.18,
-      Na: 22.99,
-      Mg: 24.305,
-      Al: 26.982,
-      Si: 28.085,
-      P: 30.974,
-      S: 32.07,
-      Cl: 35.45,
-      Ar: 39.948,
-      K: 39.098,
-      Ca: 40.078,
-      Sc: 44.956,
-      Ti: 47.867,
-      V: 50.942,
-      Cr: 51.996,
-      Mn: 54.938,
-      Fe: 55.845,
-      Co: 58.933,
-      Ni: 58.693,
-      Cu: 63.546,
-      Zn: 65.38,
-      Ga: 69.723,
-      Ge: 72.63,
-      As: 74.922,
-      Se: 78.971,
-      Br: 79.904,
-      Kr: 83.798,
-      Rb: 85.468,
-      Sr: 87.62,
-      Y: 88.906,
-      Zr: 91.224,
-      Nb: 92.906,
-      Mo: 95.95,
-      Tc: 98,
-      Ru: 101.07,
-      Rh: 102.91,
-      Pd: 106.42,
-      Ag: 107.87,
-      Cd: 112.41,
-      In: 114.82,
-      Sn: 118.71,
-      Sb: 121.76,
-      Te: 127.6,
-      I: 126.9,
-      Xe: 131.29,
-      Cs: 132.91,
-      Ba: 137.33,
-      La: 138.91,
-      Ce: 140.12,
-      Pr: 140.91,
-      Nd: 144.24,
-      Pm: 145,
-      Sm: 150.36,
-      Eu: 151.96,
-      Gd: 157.25,
-      Tb: 158.93,
-      Dy: 162.5,
-      Ho: 164.93,
-      Er: 167.26,
-      Tm: 168.93,
-      Yb: 173.05,
-      Lu: 174.97,
-      Hf: 178.49,
-      Ta: 180.95,
-      W: 183.84,
-      Re: 186.21,
-      Os: 190.23,
-      Ir: 192.22,
-      Pt: 195.08,
-      Au: 196.97,
-      Hg: 200.59,
-      Tl: 204.38,
-      Pb: 207.2,
-      Bi: 208.98,
-      Po: 209,
-      At: 210,
-      Rn: 222,
-      Fr: 223,
-      Ra: 226,
-      Ac: 227,
-      Th: 232.04,
-      Pa: 231.04,
-      U: 238.03,
-      Np: 237,
-      Pu: 244,
-      Am: 243,
-      Cm: 247,
-      Bk: 247,
-      Cf: 251,
-      Es: 252,
-      Fm: 257,
-      Md: 258,
-      No: 259,
-      Lr: 266,
-      Rf: 267,
-      Db: 270,
-      Sg: 271,
-      Bh: 270,
-      Hs: 277,
-      Mt: 278,
-      Ds: 281,
-      Rg: 282,
-      Cn: 285,
-      Nh: 286,
-      Fl: 289,
-      Mc: 290,
-      Lv: 293,
-      Ts: 294,
-      Og: 294,
-    };
-
     const parsed = this.parseFormula(formula);
     return parsed.reduce((sum, [element, count]) => {
       return sum + (periodicTable[element] || 0) * count;
@@ -421,8 +273,7 @@ export default class Interpretor {
     const n = this.ensureNumber(moles, "moles");
     const T = this.ensureNumber(temperature, "temperature");
     const P = this.ensureNumber(pressure, "pressure");
-    const R = 0.0821; // ideal gas constant in L·atm/(mol·K)
-    return (n * R * T) / P;
+    return (n * physicalConstants.R * T) / P;
   }
 
   ensureNumber(value, name) {
@@ -450,66 +301,6 @@ export default class Interpretor {
 
   findRedoxAgents(compounds) {
     // Split the reaction string (by '+') and filter for known agents.
-    const oxidizingAgents = [
-      "O2", // Oxygen
-      "O3", // Ozone
-      "H2O2", // Hydrogen Peroxide
-      "KMnO4", // Potassium Permanganate
-      "HNO3", // Nitric Acid
-      "K2Cr2O7", // Potassium Dichromate
-      "CrO3", // Chromium Trioxide
-      "H2SO4", // Sulfuric Acid (concentrated)
-      "Cl2", // Chlorine
-      "Br2", // Bromine
-      "I2", // Iodine
-      "FeCl3", // Ferric Chloride
-      "CuSO4", // Copper(II) Sulfate
-      "MnO2", // Manganese Dioxide
-      "AgNO3", // Silver Nitrate
-      "NaOCl", // Sodium Hypochlorite (Bleach)
-      "PbO2", // Lead(IV) Oxide
-      "N2O", // Nitrous Oxide
-      "NO2", // Nitrogen Dioxide
-      "HClO", // Hypochlorous Acid
-      "ClO2", // Chlorine Dioxide
-      "HBrO", // Hypobromous Acid
-      "HIO3", // Iodic Acid
-      "XeF4", // Xenon Tetrafluoride
-      "C2H5NO2", // Glycine Nitrate
-    ];
-
-    const reducingAgents = [
-      "H2", // Hydrogen Gas
-      "C", // Carbon (Graphite)
-      "CO", // Carbon Monoxide
-      "CH4", // Methane
-      "C2H6", // Ethane
-      "C6H6", // Benzene
-      "Fe", // Iron
-      "Zn", // Zinc
-      "Al", // Aluminum
-      "Mg", // Magnesium
-      "Na", // Sodium
-      "K", // Potassium
-      "Li", // Lithium
-      "Ca", // Calcium
-      "Ba", // Barium
-      "Sn", // Tin
-      "Pb", // Lead
-      "NH3", // Ammonia
-      "H2S", // Hydrogen Sulfide
-      "NaBH4", // Sodium Borohydride
-      "LiAlH4", // Lithium Aluminum Hydride
-      "SO2", // Sulfur Dioxide
-      "FeSO4", // Iron(II) Sulfate
-      "TiCl3", // Titanium(III) Chloride
-      "Cu", // Copper
-      "Cr2O3", // Chromium(III) Oxide
-      "AsH3", // Arsine
-      "SbH3", // Stibine
-      "PH3", // Phosphine
-    ];
-
     return {
       oxidizing: compounds
         .split("+")
@@ -525,8 +316,7 @@ export default class Interpretor {
   isCombustionReaction(reactants, products, requiredProducts) {
     // Check that one valid fuel is present, along with O2 on the reactant side.
     const reactantParts = reactants.split("+").map((s) => s.trim());
-    const validFuels = ["CH4", "C2H6", "C6H6"];
-    const hasFuel = reactantParts.some((part) => validFuels.includes(part));
+    const hasFuel = reactantParts.some((part) => validCombustionFuels.includes(part));
     const hasO2 = reactantParts.includes("O2");
 
     // Check that every required product (CO2, H2O) is present.
@@ -535,6 +325,15 @@ export default class Interpretor {
       productParts.includes(p)
     );
     return hasFuel && hasO2 && hasAllProducts;
+  }
+
+  // Functions for checking whether the substance is Acid or Base
+  isAcid(compound) {
+    return acids.includes(compound);
+  }
+  
+  isBase(compound) {
+    return bases.includes(compound);
   }
 
   validateNumberOperands(left, right, operator) {
