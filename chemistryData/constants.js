74e86d84dfc3b024774e6b4b9645a7ac91969733
Patricia Moraru <patriciamoraruv@gmail.com>
2025-04-22 21:52:49 +0300

    Deletion of unnecessary files

--- chemistryData/constants.js
+++
@@ -1,6 +0,0 @@
-export const physicalConstants = {
-    R: 0.0821, // Ideal gas constant in L·atm/(mol·K)
-    standardTemperature: 273.15, // Kelvin
-    standardPressure: 1, // atm
-    molarVolume: 22.4, // L/mol at STP
-  };
